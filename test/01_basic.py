e50dce77a4195886d8f90aeb9932ab6e4de1f7de
Ju Ouyang <juouayang@aicots.com>
2021-04-14 18:21:42 +0800

    Fix create bug

--- test/01_basic.py
+++ test/01_basic.py
@@ -31,6 +31,25 @@ def test_get_strategies_by_user_equals_200():
 @pytest.mark.order(3)
 def test_create_new_strategy():
     response = requests.post("http://admin:85114481@127.0.0.1:5000/api/v1.0/strategies",
-                                data={'name': "my_strategy_c"},
-                                headers={'Content-Type': 'application/json'}
-                            )
+                                data='{"name": "my_strategy_c"}',
+                                headers={'Content-Type': 'application/json'})
+    assert response.headers["Content-Type"] == "application/json"
+    assert response.status_code == 201
+
+    response = requests.post("http://admin:85114481@127.0.0.1:5000/api/v1.0/strategies",
+                                data='{"name": "my_strategy_c"}',
+                                headers={'Content-Type': 'application/json'})
+    assert response.headers["Content-Type"] == "application/json"
+    assert response.status_code == 429
+
+    response = requests.get("http://admin:85114481@127.0.0.1:5000/api/v1.0/strategies/all")
+    assert response.headers["Content-Type"] == "application/json"
+    assert response.status_code == 200
+    resp_body = response.json()
+    assert len(resp_body['strategies']) == 4
+
+    response = requests.get("http://admin:85114481@127.0.0.1:5000/api/v1.0/strategies")
+    assert response.headers["Content-Type"] == "application/json"
+    assert response.status_code == 200
+    resp_body = response.json()
+    assert len(resp_body['strategies']) == 3
