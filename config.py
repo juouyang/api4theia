1796fa98ee4c1d385f5ab97c7a3bb384d8af85c0
Ju Ouyang <juouayang@aicots.com>
2021-05-26 14:52:19 +0800

    Fix .gitignore not working

--- config.py
+++ config.py
@@ -5,46 +5,45 @@ class Config:
     DB = 'Json'
     SECRET_KEY = os.environ.get("SECRET_KEY") or '85114481'
     DOCKER_IMAGE = "theia-python:aicots"
-    GIT_IGNORE = '''
-    /.pytest_cache/
-    __pycache__/
-    *.py[cod]
-    /venv/
-    /.vscode/
-    **/log*
-    **/.theia
-    **/.sandbox
-    **/.Trash*
-    '''
+    GIT_IGNORE = (
+        "/.pytest_cache/\n"
+        "__pycache__/\n"
+        "*.py[cod]\n"
+        "/venv/\n"
+        "/.vscode/\n"
+        "**/log*\n"
+        "**/.theia\n"
+        "**/.sandbox\n"
+        "**/.Trash*\n"
+    )
     GIT_INIT = "git init;git config user.email 'root@local';git add ./*;git add .gitignore;git commit -m 'first commit'"
-    MATPLOTLIB_SAMPLE = '''
-    import matplotlib
-    import matplotlib.pyplot as plt
-    import numpy as np
-    t = np.arange(0.0, 2.0, 0.01)
-    s = 1 + np.sin(2 * np.pi * t)
-    fig, ax = plt.subplots()
-    ax.plot(t, s)
-    ax.set(xlabel='time (s)', ylabel='voltage (mV)', title='About as simple as it gets, folks')
-    ax.grid()
-    fig.savefig("plot.png")
-    '''
-    PLOTLY_SAMPLE = '''
-    import plotly.graph_objects as go
-    import pandas as pd
-    from datetime import datetime
-    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
-    fig = go.Figure(data=[go.Candlestick(x=df['Date'],
-                    open=df['AAPL.Open'],
-                    high=df['AAPL.High'],
-                    low=df['AAPL.Low'],
-                    close=df['AAPL.Close'],
-                    increasing_line_color= 'red',
-                    decreasing_line_color= 'green')])
-    fig.update_layout(hovermode='x')
-    fig.show()
-    '''
-
+    MATPLOTLIB_SAMPLE = (
+        "import matplotlib\n"
+        "import matplotlib.pyplot as plt\n"
+        "import numpy as np\n"
+        "t = np.arange(0.0, 2.0, 0.01)\n"
+        "s = 1 + np.sin(2 * np.pi * t)\n"
+        "fig, ax = plt.subplots()\n"
+        "ax.plot(t, s)\n"
+        "ax.set(xlabel='time (s)', ylabel='voltage (mV)', title='About as simple as it gets, folks')\n"
+        "ax.grid()\n"
+        "fig.savefig(\"plot.png\")\n"
+    )
+    PLOTLY_SAMPLE = (
+        "import plotly.graph_objects as go\n"
+        "import pandas as pd\n"
+        "from datetime import datetime\n"
+        "df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')\n"
+        "fig = go.Figure(data=[go.Candlestick(x=df['Date'],\n"
+        "               open=df['AAPL.Open'],\n"
+        "                high=df['AAPL.High'],\n"
+        "                low=df['AAPL.Low'],\n"
+        "                close=df['AAPL.Close'],\n"
+        "                increasing_line_color= 'red',\n"
+        "                decreasing_line_color= 'green')])\n"
+        "fig.update_layout(hovermode='x')\n"
+        "fig.show()\n"
+    )
     PACK_CMD = "curl -s https://raw.githubusercontent.com/juouyang-aicots/py2docker/main/build.sh | bash"
 
     @staticmethod
