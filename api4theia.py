19b779865574a0500ff3579dcb6230ad1f45d5ba
Ju Ouyang <juouayang@aicots.com>
2021-05-27 11:51:39 +0800

    Use cheroot for WSGI server

--- api4theia.py
+++ api4theia.py
@@ -1,8 +1,17 @@
 #!venv/bin/python
 from app import create_app
 from app.docker import sync_containers_status
+from cheroot.wsgi import Server as WSGIServer
+from cheroot.wsgi import PathInfoDispatcher as WSGIPathInfoDispatcher
+from cheroot.ssl.builtin import BuiltinSSLAdapter
 
-app = create_app('default')
-sync_containers_status()
-context = (app.config['CRT_FILE'], app.config['KEY_FILE'])
-app.run(host='0.0.0.0', port=app.config['API_PORT'], debug=app.config['DEBUG'], ssl_context=context)
+if __name__ == '__main__':
+    app = create_app('default')
+    sync_containers_status()
+    d = WSGIPathInfoDispatcher({'/': app})
+    server = WSGIServer(('0.0.0.0', app.config['API_PORT']), d)
+    server.ssl_adapter =  BuiltinSSLAdapter(app.config['CRT_FILE'], app.config['KEY_FILE'], None)
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        server.stop()
