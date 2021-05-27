bdb9319fb9c2c58f91efd193189476c8fdc3c3e0
Ju Ouyang <juouayang@aicots.com>
2021-05-27 15:36:33 +0800

    Fix config load failed when using VS code debug

--- api4theia.py
+++ api4theia.py
@@ -6,19 +6,19 @@ from cheroot.wsgi import PathInfoDispatcher as WSGIPathInfoDispatcher
 from cheroot.ssl.builtin import BuiltinSSLAdapter
 import os
 
-if __name__ == '__main__':
-    flask_config = os.getenv('FLASK_CONFIG') or 'default'
-    app = create_app(flask_config)
-    sync_containers_status()
 
-    if (flask_config == 'prod'):
-        d = WSGIPathInfoDispatcher({'/': app})
-        server = WSGIServer(('0.0.0.0', app.config['API_PORT']), d)
-        server.ssl_adapter =  BuiltinSSLAdapter(app.config['CRT_FILE'], app.config['KEY_FILE'], None)
-        try:
-            server.start()
-        except KeyboardInterrupt:
-            server.stop()
-    else:
-        context = (app.config['CRT_FILE'], app.config['KEY_FILE'])
-        app.run(host='0.0.0.0', port=app.config['API_PORT'], debug=app.config['DEBUG'], ssl_context=context)
+flask_config = os.getenv('FLASK_CONFIG') or 'default'
+app = create_app(flask_config)
+sync_containers_status()
+
+if (flask_config == 'prod'):
+    d = WSGIPathInfoDispatcher({'/': app})
+    server = WSGIServer(('0.0.0.0', app.config['API_PORT']), d)
+    server.ssl_adapter =  BuiltinSSLAdapter(app.config['CRT_FILE'], app.config['KEY_FILE'], None)
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        server.stop()
+else:
+    context = (app.config['CRT_FILE'], app.config['KEY_FILE'])
+    app.run(host='0.0.0.0', port=app.config['API_PORT'], debug=app.config['DEBUG'], ssl_context=context)
