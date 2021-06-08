348d6d50dee10a72386f83f9b1449ccdcb4ebd11
Ju Ouyang <juouayang@aicots.com>
2021-06-08 09:36:29 +0800

    Update for strategy template

--- migrations/init_strategy_template.sh
+++ migrations/init_strategy_template.sh
@@ -1,4 +1,4 @@
 rm -rf ${PWD}/Doquant/Strategy
 mkdir -p ${PWD}/Doquant/Strategy
 cd ${PWD}/Doquant/Strategy
-wget -qO- https://github.com/juouyang-aicots/py2docker/raw/main/Strategy_f10a64c7.tar | tar x
+wget -qO- https://github.com/juouyang-aicots/py2docker/raw/main/Strategy_4d5de312.tar | tar x
