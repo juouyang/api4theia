fe70c8d0b8c5b09df6b15d6a03df6672fc0add3f
Ju Ouyang <juouayang@aicots.com>
2021-06-10 09:18:49 +0800

    Update strategy template by git command

--- migrations/init_strategy_template.sh
+++ migrations/init_strategy_template.sh
@@ -1,4 +1,4 @@
 rm -rf ${PWD}/Doquant/Strategy
 mkdir -p ${PWD}/Doquant/Strategy
 cd ${PWD}/Doquant/Strategy
-wget -qO- https://github.com/juouyang-aicots/py2docker/raw/main/Strategy_4d5de312.tar | tar x
+ssh-agent bash -c 'ssh-add ../../github_key_doquant; git clone --depth 1 git@github.com:ian15937/Doquant.git .; git filter-branch --prune-empty --subdirectory-filter Strategy HEAD'
